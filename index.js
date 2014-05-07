4f162cda0faf0724f25e9befb923206c1ab07a8c
komiksoj <curiouskayko@gmail.com>
2014-05-07 15:17:35 -0700

    Keyboard control works.

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ window.onload = function() {
 	var page = 1;
 	$("#comic").find("img")
 		.on("click", function() {
-			page = page + 1;
+			page++;
 			if (page == 3)
 				page = 1;
-			$("img").attr({src: "./comic/" + page + ".png"});
+			pageChange(page);
 	})
-		
+
+	$("body")
+		.keyup (function(event) {
+			if (event.which == 39) {
+				page++;
+				if (page == 3)
+					page = 1;
+				pageChange(page);
+			}
+			if (event.which == 37) {
+				page--;
+				if (page == 0)
+					page = 2;
+				pageChange(page);
+			}
+	})
+
+	function pageChange(page) {
+		$("img:first").attr({src: "./comic/" + page + ".png"});
+	}
 }
+
